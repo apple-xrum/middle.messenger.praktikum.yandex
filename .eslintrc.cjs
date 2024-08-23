68ce053621cae985c78c5faedb1e36c14af33b03
AppleXrum <156526897+App1eXrum@users.noreply.github.com>
2024-08-24 02:54:18 +0300

    ...

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,10 +17,18 @@ module.exports = {
             }
         }
     ],
+    "parser": "@typescript-eslint/parser",
     "parserOptions": {
-        "ecmaVersion": "latest",
-        "sourceType": "module"
+      "project": "./tsconfig.json",
+      "sourceType": "module"
     },
+    "plugins": [
+      "@typescript-eslint"
+    ],
+    "extends": [
+      "eslint:recommended",
+      "plugin:@typescript-eslint/recommended"
+    ],
     "rules": {
     }
 }
