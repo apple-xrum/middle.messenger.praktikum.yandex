46796053a89e78b3d75209712e562a0cf7ce977c
AppleXrum <156526897+App1eXrum@users.noreply.github.com>
2024-08-24 02:36:01 +0300

    подключил eslint

--- src/pages/script.js
+++ src/pages/script.js
@@ -1,2 +1,2 @@
-const imgUrl = new URL('../images/chat/icon-chat.svg', import.meta.url).href;
+const imgUrl = new URL('../images/chat/icon-chat.svg', import.meta.url).href
 document.querySelector('.chat__image').src = imgUrl
