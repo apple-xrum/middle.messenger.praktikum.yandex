1d1df19697c3747ba19a19e782705d235fe5f69e
AppleXrum <156526897+App1eXrum@users.noreply.github.com>
2024-08-25 12:18:14 +0300

    добавлено поле display_name

--- vite.config.js
+++ vite.config.js
@@ -119,12 +119,12 @@ export default defineConfig({
               type: "text",
               value: "Константин"
             },
-            // {
-            //   name: "chat_name",
-            //   label: "Имя в чате",
-            //   type: "text",
-            //   value: "AppleXrum"
-            // },
+            {
+              name: "display_name",
+              label: "Имя в чате",
+              type: "text",
+              value: "AppleXrum"
+            },
             {
               name: "phone",
               label: "Телефон",
