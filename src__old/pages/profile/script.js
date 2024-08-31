9452b53177f14c5e0c49bb3193c673de7164bf6d
Slastikhin Konstantin <Apple.Xrum@yandex.ru>
2024-08-31 20:12:54 +0300

    удалена старая папка src_old

--- src__old/pages/profile/script.js
+++
@@ -1,2 +0,0 @@
-const imgUrl = new URL('../../images/profile/avatar.svg', import.meta.url).href
-document.querySelector('.profile__image').src = imgUrl
--- src__old/pages/script.js
+++
@@ -1,2 +0,0 @@
-const imgUrl = new URL('../images/chat/icon-chat.svg', import.meta.url).href
-document.querySelector('.chat__image').src = imgUrl
