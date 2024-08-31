14c5e6bb238b97910f0557e1d2987c89c25e4401
Slastikhin Konstantin <Apple.Xrum@yandex.ru>
2024-09-01 01:34:08 +0300

    правка на netlify

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import path, { resolve } from 'path';
 import { defineConfig } from 'vite';
-import { fileURLToPath } from 'url';
-
-const dirname = path.dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   root: '.',
   build: {
-    outDir: resolve(dirname, 'dist'),
+    outDir: 'dist',
   },
 });
