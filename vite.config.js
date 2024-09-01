714bde5f3c2f3a5e345074368b59e1430b4ad3e1
Slastikhin Konstantin <Apple.Xrum@yandex.ru>
2024-09-01 09:25:48 +0300

    добавлены страницы для netlify

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,15 @@ export default defineConfig({
   root: '.',
   build: {
     outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        profile: 'index.html',
+        signin: 'index.html',
+        signup: 'index.html',
+        "404": 'index.html',
+        "500": 'index.html',
+      },
+    },
   },
 });
