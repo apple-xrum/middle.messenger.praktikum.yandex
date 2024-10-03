caa9c46bc2289910f6159bb0b0101a9f2f7785c3
Slastikhin Konstantin <Apple.Xrum@yandex.ru>
2024-10-03 03:06:38 +0300

    Добавлен роутинг по изменению url

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Handlebars from "handlebars";
 import * as Components from "./components";
 import * as Pages from "./pages";
+import Router from "./core/Router";
 
 declare global {
   export type Keys<T extends Record<string, unknown>> = keyof T;
@@ -16,140 +17,22 @@ const pages = {
   pageServerError: [Pages.PageServerError],
 };
 
-const components = {
-  Sign: [Components.Sign],
-  Form: [Components.Form],
-  FormField: [Components.FormField],
-  FormInput: [Components.FormInput],
-  FormSubmit: [Components.FormSubmit],
-  Sidebar: [Components.Sidebar],
-  ChatList: [Components.ChatList],
-  ChatItem: [Components.ChatItem],
-  Chat: [Components.Chat],
-  ChatFooter: [Components.ChatFooter],
-  ProfileField: [Components.ProfileField],
-  ProfileInput: [Components.ProfileInput],
-  ProfileButton: [Components.ProfileButton],
-  ProfileSubmit: [Components.ProfileSubmit],
-  ProfileLink: [Components.ProfileLink],
-  ErrorPartial: [Components.ErrorPartial],
-}
-
-Object.entries(components).forEach(([name, component]) => {
+Object.entries(Components).forEach(([name, component]) => {
   // eslint-disable-next-line @typescript-eslint/ban-ts-comment
   // @ts-ignore
   Handlebars.registerPartial(name, component);
 });
 
-function navigate(page: string) {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  const [source, context] = pages[page];
-  const container = document.getElementById("app")!;
+const router = new Router("#app");
 
-  if (source instanceof Object) {
-    // eslint-disable-next-line @typescript-eslint/no-shadow, new-cap
-    const page = new source(context);
-    container.innerHTML = "";
-    container.append(page.getContent());
-    // page.dispatchComponentDidMount();
-    return;
-  }
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+window.router = router;
 
-  container.innerHTML = Handlebars.compile(source)(context);
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  const searchString = new URLSearchParams(window.location.search)
-  const route = searchString.get('route');
-  if(route) {
-    switch (route) {
-      case "/": {
-        navigate("home");
-        break;
-      }
-      case "signin": {
-        navigate("signin");
-        break;
-      }
-      case "signup": {
-        navigate("signup");
-        break;
-      }
-      case "profile": {
-        navigate("profile");
-        break;
-      }
-      case "500": {
-        navigate("pageServerError");
-        break;
-      }
-      default: {
-        navigate("pageNotFound");
-        break;
-      }
-    }
-    return
-  }
-
-  switch (window.location.pathname) {
-    case "/": {
-      navigate("home");
-      break;
-    }
-    case "/signin/": {
-      navigate("signin");
-      break;
-    }
-    case "/signup/": {
-      navigate("signup");
-      break;
-    }
-    case "/profile/": {
-      navigate("profile");
-      break;
-    }
-    case "/500/": {
-      navigate("pageServerError");
-      break;
-    }
-    default: {
-      navigate("pageNotFound");
-      break;
-    }
-  }
-});
-
-document.addEventListener("click", (event: Event) => {
-  if(event.target && (event.target as HTMLAnchorElement).tagName === "A"){
-    event.preventDefault();
-    const link = event.target as HTMLAnchorElement;
-    const href = link.getAttribute("href");
-    switch (href) {
-      case "/": {
-        navigate("home");
-        break;
-      }
-      case "/signin/": {
-        navigate("signin");
-        break;
-      }
-      case "/signup/": {
-        navigate("signup");
-        break;
-      }
-      case "/profile/": {
-        navigate("profile");
-        break;
-      }
-      case "/500/": {
-        navigate("pageServerError");
-        break;
-      }
-      default: {
-        navigate("pageNotFound");
-        break;
-      }
-    }
-  }
-})
+router
+  .use('/', Pages.SignInPage)
+  .use('/sign-up', Pages.SignUpPage)
+  .use('/settings', Pages.ProfilePage)
+  .use('/messenger', Pages.HomePage)
+  .use('*', Pages.PageNotFound)
+  .start();
